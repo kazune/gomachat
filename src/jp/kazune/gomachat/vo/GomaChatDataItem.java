82fd38108607de58a4639077c9a31a5d61ff2648
kazune iga <kazune.iga@gmail.com>
2015-11-08 09:50:13 +0900

    time

--- src/jp/kazune/gomachat/vo/GomaChatDataItem.java
+++ src/jp/kazune/gomachat/vo/GomaChatDataItem.java
@@ -1,11 +1,29 @@
 package jp.kazune.gomachat.vo;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class GomaChatDataItem {
 	protected String iconPath = "http://www.yuyushiki.net/core_sys/images/main/cont/chara/chara_navi2.gif";
 
 	protected String userName = "haramura";
 
 	protected String chatText = "いつか、鳥になりたい・・・・unk";
+
+	protected Date chatDate = new Date();
+
+	public Date getChatDate() {
+		return chatDate;
+	}
+
+	public String getChatDateText() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:");
+		return sdf.format(chatDate);
+	}
+
+	public void setChatDate(Date chatDate) {
+		this.chatDate = chatDate;
+	}
 
 	public String getChatText() {
 		return chatText;
